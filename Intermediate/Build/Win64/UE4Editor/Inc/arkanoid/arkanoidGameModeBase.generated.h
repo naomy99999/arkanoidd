4b50b255997c04e052394586370392bb91514d2c
Naomy Yailín Paredes Paredes <114457380+naomy99999@users.noreply.github.com>
2022-11-25 15:21:10 -0600

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/arkanoid/arkanoidGameModeBase.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARKANOID_arkanoidGameModeBase_generated_h
+#error "arkanoidGameModeBase.generated.h already included, missing '#pragma once' in arkanoidGameModeBase.h"
+#endif
+#define ARKANOID_arkanoidGameModeBase_generated_h
+
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_SPARSE_DATA
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_RPC_WRAPPERS
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_RPC_WRAPPERS_NO_PURE_DECLS
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAarkanoidGameModeBase(); \
+	friend struct Z_Construct_UClass_AarkanoidGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AarkanoidGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/arkanoid"), NO_API) \
+	DECLARE_SERIALIZER(AarkanoidGameModeBase)
+
+
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_INCLASS \
+private: \
+	static void StaticRegisterNativesAarkanoidGameModeBase(); \
+	friend struct Z_Construct_UClass_AarkanoidGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AarkanoidGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/arkanoid"), NO_API) \
+	DECLARE_SERIALIZER(AarkanoidGameModeBase)
+
+
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AarkanoidGameModeBase(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AarkanoidGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AarkanoidGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AarkanoidGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AarkanoidGameModeBase(AarkanoidGameModeBase&&); \
+	NO_API AarkanoidGameModeBase(const AarkanoidGameModeBase&); \
+public:
+
+
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AarkanoidGameModeBase(AarkanoidGameModeBase&&); \
+	NO_API AarkanoidGameModeBase(const AarkanoidGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AarkanoidGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AarkanoidGameModeBase); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AarkanoidGameModeBase)
+
+
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_PRIVATE_PROPERTY_OFFSET
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_13_PROLOG
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_PRIVATE_PROPERTY_OFFSET \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_SPARSE_DATA \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_RPC_WRAPPERS \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_INCLASS \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_PRIVATE_PROPERTY_OFFSET \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_SPARSE_DATA \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_INCLASS_NO_PURE_DECLS \
+	arkanoid_Source_arkanoid_arkanoidGameModeBase_h_16_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARKANOID_API UClass* StaticClass<class AarkanoidGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID arkanoid_Source_arkanoid_arkanoidGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
